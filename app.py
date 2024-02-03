fe33ce9700dd3753e67537233ad0ab39cf4c5d6b
Shreyansh <bugbreaker18@gmail.com>
2024-02-03 15:06:33 +0530

    store applications in database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,jsonify, request
-from database import load_jobs_from_db, load_job_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 
 app = Flask(__name__)
@@ -29,9 +29,16 @@ def show_job(id):
         return "Not Found", 404
     return render_template('jobpage.html', job = job)
 
-@app.route("/job/<id>/apply")
+@app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
-    data = request.args
+    data = request.form
+    job = load_job_from_db(id)
+    add_application_to_db(id, data)
+    #store this in db
+    #give an acknowledgement
+    return render_template('application_submitted.html',
+                           application=data,
+                           job = job)
     
 
 
--- database.py
+++ database.py
@@ -26,3 +26,19 @@ def load_job_from_db(id):
             return None
         else:
             return rows[0]._asdict()
+        
+
+def add_application_to_db(job_id, application):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications(job_id, full_name, email, linkedin_url, educational_background, work_experience, resume_url) VALUES(:job_id, :full_name, :email, :linkedin_url, :educational_background, :work_experience, :resume_url)")
+        params = {
+            "job_id": job_id,
+            "full_name": application['full_name'],
+            "email": application['email'],
+            "linkedin_url": application['linkedin_url'],
+            "educational_background": application['educational_background'],
+            "work_experience": application['work_experience'],
+            "resume_url": application['resume_url']
+        }
+        conn.execute(query, params)
+
